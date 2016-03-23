38b44aa0ebce5906c25d2198454f47ccd8200909
Martin Prace <martinmatejka@martin-macbook-pro.local>
2016-03-23 13:43:04 +0100

    Hotovo

--- Pricitacka/ViewController.swift
+++ Pricitacka/ViewController.swift
@@ -12,14 +12,90 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+      
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Outlets
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var nazevHry: UIImageView!
+    @IBOutlet weak var pocetNavyseni: UITextField!
+    @IBOutlet weak var restartBtn: UIButton!
+    
+    
+    @IBOutlet weak var prictiBtn: UIButton!
+    @IBOutlet weak var vypocet: UILabel!
+    
+    //Variables
+    
+    var navyseni: Int = 0
+    var aktualniCislo: Int = 0
+    
+    
 
-
+    @IBAction func zmacknutiPlay(sender: UIButton) {
+        
+        
+        
+       if pocetNavyseni.text != nil && pocetNavyseni.text != "" {
+        navyseni = Int(pocetNavyseni.text!)!
+        playBtn.hidden = true
+        nazevHry.hidden = true
+        pocetNavyseni.hidden = true
+        
+        
+        prictiBtn.hidden = false
+        vypocet.hidden = false
+        restartBtn.hidden = false
+        updateVysledek()
+        
+       }
+        
+        
+    }
+    
+    @IBAction func pricti(sender: UIButton) {
+        aktualniCislo = aktualniCislo + navyseni
+        updateVysledek()
+        
+        
+        
+    }
+    
+    
+    @IBAction func restartBtn(sender: AnyObject) {
+        restartGame()
+        
+    }
+    
+    func restartGame(){
+        prictiBtn.hidden = true
+        vypocet.hidden = true
+        restartBtn.hidden = true
+        
+        playBtn.hidden = false
+        nazevHry.hidden = false
+        pocetNavyseni.hidden = false
+        
+        pocetNavyseni.text = ""
+        aktualniCislo = 0
+        
+    }
+    
+    func updateVysledek(){
+        let vysledek = aktualniCislo + navyseni
+        vypocet.text = "\(aktualniCislo) + \(navyseni) = \(vysledek) "
+    }
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
 }
 
